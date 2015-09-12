52c69e38dc8ddd0b4bc803a8250588bd995e1d6e
Kenny Joseph <kenn9j@gmail.com>
2015-09-12 17:02:02 +0700

    wd class has reference to saltyDuck now (tests pending)

--- tests/webdriver/webdriver.spec.js
+++ tests/webdriver/webdriver.spec.js
@@ -1,3 +1,17 @@
 /**
  * Created by kenny on 23/08/15.
  */
+
+describe('SaltyWebDriver', function(){
+
+  describe('.initBrowser', function(){
+
+    it('should open a browser from default settings', function(){
+
+
+
+    });
+
+  });
+
+});
